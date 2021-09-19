423bcfdd37436f9e3d18ff917bc955546e4a6ec7
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-09-19 11:04:08 +0200

    moving on to TS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Web3 from 'web3'
+import "./App.css";
+import Web3 from "web3";
+import { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
+import Typography from '@material-ui/core/Typography'
 function App() {
+  const url = "https://mainnet.infura.io/v3/e31d6b31abd249718714f1244b8628e7 ";
+  const web3 = new Web3(url);
+  let address = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
+  const [balance, setBalance] = useState(0);
+  const howMuch = () => {
+    web3.eth.getBalance(address, (err, bal) => {
+      setBalance(web3.utils.fromWei(bal, 'ether'));
+      
+    });
+  };
+  useEffect(() => {
+    console.log(typeof(balance));
+    
+  }, [balance]);
   return (
-    <h1>Hey!</h1>
-
+    <div>
+      <Typography variant="h1" color="initial">{`${balance}`}</Typography> 
+      <Button onClick={howMuch}>rich?</Button>
+    </div>
   );
-}
-
+} 
 export default App;
+ 
